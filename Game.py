abf8e98563b3e2171b435b7b5e3d3f8186bd2d09
Strefeler Michael <mstrefel@intranet.epfl.ch>
2018-11-16 08:42:46 +0100

    Fixed pylint errors and changes variable name

--- Game.py
+++ Game.py
@@ -17,30 +17,30 @@ outcomes = {
 
 play = True
 
-while play == True:
+while play is True:
 
     computer = choice(options)
-    user_input = input('Please select; Rock, Paper, Scissors, Lizard or Spock\n')
-    player = user_input.lower().capitalize()
+    roll = input('Please select; Rock, Paper, Scissors, Lizard or Spock\n')
+    roll = roll.lower().capitalize()
 
     # Makes sure that player makes valid choice
-    while player not in options:
-        player = input('Please select; Rock, Paper, Scissors, Lizard or Spock\n')
+    while roll not in options:
+        roll = input('Please select; Rock, Paper, Scissors, Lizard or Spock\n')
 
-    print(f'Player: {player}')
+    print(f'Player: {roll}')
     print(f'Computer: {computer}')
 
-    if (player, computer) in outcomes:
-        print(f'You win! {player} {outcomes[player, computer]} {computer}.')
-    elif (computer, player) in outcomes:
-        print(f'You lose! {computer} {outcomes[computer, player]} {player}.')
+    if (roll, computer) in outcomes:
+        print(f'You win! {roll} {outcomes[roll, computer]} {computer}.')
+    elif (computer, roll) in outcomes:
+        print(f'You lose! {computer} {outcomes[computer, roll]} {roll}.')
     else:
-        print('Tie!')  
+        print('Tie!')
 
     print('Would you like to play again? (Y/N)')
     answer = input()
 
-    if answer.lower() =='y' or answer.lower() =='yes':
-        play == True
+    if answer.lower() == 'y' or answer.lower() == 'yes':
+        play = True
     else:
         break
